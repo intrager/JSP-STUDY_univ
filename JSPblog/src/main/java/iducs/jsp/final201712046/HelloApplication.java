c119e0977e5dc41be53cf3bca8ded21c1005fec5
intrager <intrager@naver.com>
2021-12-08 23:51:52 +0900

    프로젝트 생성

---
+++ JSPblog/src/main/java/iducs/jsp/final201712046/HelloApplication.java
@@ -0,0 +1,9 @@
+package iducs.jsp.final201712046;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
